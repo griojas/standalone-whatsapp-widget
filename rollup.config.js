64ec845af312122b34e8bbc2f3763acbd7e59f2b
Guillermo Riojas <guillermo.riojas@fresha.com>
2022-10-28 09:50:29 +0200

    Updated code

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,8 @@ import { babel } from '@rollup/plugin-babel'
 const path = require('path')
 const pkg = require(path.resolve(__dirname, 'package.json'))
 const copyleft = `/*!
-  * WhatsApp Widget v${pkg.version} (c) 2020 - ${pkg.author.name}
-  * Contributors (https://github.com/agraris/whatsapp-widget/graphs/contributors)
-  * Licensed under MIT (https://github.com/agraris/whatsapp-widget/blob/master/LICENSE)
+  * WhatsApp Widget v${pkg.version} (c) 2022 - ${pkg.author.name}
+  * Licensed under MIT (https://github.com/griojas/whatsapp-widget.git/blob/main/LICENSE)
   * WhatsApp Widget does not affiliate with WhatsApp Inc. in any way.
   */`
 
